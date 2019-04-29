fcf42231a6de976ebddc32660cee66eccd900548
Wilder Olmos Bernal <wolmos@ext.laliga.es>
2019-04-29 13:12:15 +0200

    Just adding a Margind and some styles on the button

--- src/components/InputWidget/styles.js
+++ src/components/InputWidget/styles.js
@@ -6,16 +6,20 @@ export const InputWrapper = styled.div`
     flex-direction: row;
     box-shadow: 0px 0px 25px 1px rgba(50, 50, 50, 0.1);
     border-radius: 16px;
+    margin: 16px;
     .city-input {
         width: 100%;
         height: ${(props) => props.height ? props.height : 56}px;
         border-radius: 16px 0 0 16px;
+        font-size: 24px;
+        padding: 0 8px;
     }
     
     .button {
         width: 80px;
         height: ${(props) => props.height ? props.height : 56}px;
         border-radius: 0 16px 16px 0;
-        background-color:${ theme.colors.$color6 }
+        background-color:${ theme.colors.$color6 };
+        font-size: 18px;
     }
 `
